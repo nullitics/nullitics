9be3b514680eb29bb1cf4cf31145fef2a5ea5a41
Serge Zaitsev <zaitsev.serge@gmail.com>
2021-01-09 22:32:16 +0100

    add a few helpers to convert requests into hits

--- hit.go
+++ hit.go
@@ -11,6 +11,18 @@ import (
 	"time"
 )
 
+const (
+	Mobile  = "mobile"
+	Desktop = "desktop"
+)
+
+var (
+	// Request headers, containing the real user IP address
+	ipHeaders = []string{"X-Real-IP", "X-Forwarded-For"}
+	// User-Agent substrings, typical only for mobile devices
+	mobileUAs = []string{"iPhone", "iPad", "Android"}
+)
+
 type Hit struct {
 	Timestamp time.Time
 	URI       string
@@ -20,22 +32,31 @@ type Hit struct {
 	Device    string
 }
 
+// Device returns the device type from the request.
+// It checks for the "d" query parameter, which would be display width in pixels.
+// Devices larger than 992px are considered desktops (break point taken from Bootstrap).
+// If "d" parameter is not present - it checks for the User-Agent for certain mobile keywords.
 func device(r *http.Request) string {
-	if d, _ := strconv.Atoi(r.URL.Query().Get("d")); d > 0 {
+	if d, _ := strconv.Atoi(r.FormValue("d")); d > 0 {
 		if d < 992 {
-			return "mobile"
+			return Mobile
 		}
-		return "desktop"
+		return Desktop
 	}
 	ua := r.UserAgent()
-	if strings.Contains(ua, "iPhone") || strings.Contains(ua, "iPad") || strings.Contains(ua, "Android") {
-		return "mobile"
+	for _, mobile := range mobileUAs {
+		if strings.Contains(ua, mobile) {
+			return Mobile
+		}
 	}
-	return "desktop"
+	return Desktop
 }
 
+// IP returns the (most likely) real user IP address. Nullitics does not store
+// any of the IP addresses, however they may be used to detect user location
+// and identify sessions.
 func ip(r *http.Request) string {
-	for _, hdr := range []string{"X-Real-IP", "X-Forwarded-For", "CF-Connecting-IP"} {
+	for _, hdr := range ipHeaders {
 		if fields := strings.Fields(r.Header.Get(hdr)); len(fields) > 0 {
 			if ip := strings.TrimRight(fields[0], ","); ip != "" {
 				return ip
@@ -46,10 +67,26 @@ func ip(r *http.Request) string {
 	return ip
 }
 
-func session(r *http.Request, salt string) string {
-	s := ip(r) + date(Now()).Format("20060102") + salt
+// Session returns a hash of the user IP address, user agent, current date and
+// a salt string. It is unique enough for most typical cases, and does not
+// violate user's privacy since no personal data is stored within a session.
+func session(ip string, salt string) string {
+	s := ip + date(Now()).Format("20060102") + salt
 	hash := md5.Sum([]byte(s))
 	return hex.EncodeToString(hash[:4])
+}
+
+func lang(r *http.Request) string {
+	for _, lang := range strings.Split(r.Header.Get("Accept-Language"), ",") {
+		if !strings.Contains(lang, ";") {
+			if len(lang) == 2 {
+				return strings.ToUpper(lang)
+			} else if len(lang) == 5 && lang[2] == '-' {
+				return strings.ToUpper(lang[3:])
+			}
+		}
+	}
+	return ""
 }
 
 func API(r *http.Request, salt string) *Hit {
@@ -58,30 +95,38 @@ func API(r *http.Request, salt string) *Hit {
 		return &Hit{}
 	}
 
-	hit := &Hit{Timestamp: Now(), URI: u.Path, Session: session(r, salt)}
+	ipaddr := ip(r)
+	hit := &Hit{Timestamp: Now(), URI: u.Path, Session: session(ipaddr, salt)}
 
 	// Referrer can be passed via "r" of the referrer, or "utm_source" of the actual page
-	if ref := r.URL.Query().Get("r"); ref != "" {
+	if ref := r.FormValue("r"); ref != "" {
 		hit.Ref = ref
 	} else if ref := u.Query().Get("utm_source"); ref != "" {
 		hit.Ref = ref
 	}
 	hit.Country = u.Query().Get("c")
+	if hit.Country == "" {
+		hit.Country = GeoDB.Find(ipaddr)
+		if hit.Country == "" {
+			hit.Country = lang(r)
+		}
+	}
 	hit.Device = device(r)
 	return hit
 }
 
 func Page(r *http.Request, salt string) *Hit {
 	path := r.URL.Path
-	ref := r.URL.Query().Get("utm_source")
+	ref := r.FormValue("utm_source")
 	if ref == "" {
 		ref = r.Referer()
 	}
-	cn := "" // TODO: use from IP
+	ipaddr := ip(r)
+	cn := lang(r)
 	return &Hit{
 		Timestamp: time.Now(),
 		URI:       path,
-		Session:   session(r, salt),
+		Session:   session(ipaddr, salt),
 		Ref:       ref,
 		Country:   cn,
 		Device:    device(r),
