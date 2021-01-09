e2b8d96b4a9000bdd687d2074d6cffb1e358555d
Serge Zaitsev <zaitsev.serge@gmail.com>
2021-01-09 10:34:53 +0100

    add daily append-only logger

---
+++ log.go
@@ -0,0 +1,136 @@
+package nullitics
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Appender struct {
+	f     *os.File
+	start time.Time
+	sb    strings.Builder
+}
+
+func NewAppender(filename string, truncate bool) (*Appender, error) {
+	flags := os.O_RDWR | os.O_CREATE
+	if truncate {
+		flags = flags | os.O_TRUNC
+	}
+	_ = os.MkdirAll(filepath.Dir(filename), 0777)
+	f, err := os.OpenFile(filename, flags, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read timestamp, if any. Start time is zero if the log file is empty
+	buf := make([]byte, 64)
+	n, err := f.Read(buf)
+	if n == 0 && err == io.EOF {
+		return &Appender{f: f}, nil
+	} else if err != nil {
+		return nil, err
+	}
+	unix := int64(0)
+	for i := 0; i < n; i++ {
+		if buf[i] >= '0' && buf[i] <= '9' {
+			unix = unix*10 + int64(buf[i]-'0')
+		} else {
+			break
+		}
+	}
+
+	// Jump to the end of the file for appending
+	if _, err := f.Seek(0, io.SeekEnd); err != nil {
+		return nil, err
+	}
+
+	return &Appender{f: f, start: time.Unix(unix, 0)}, nil
+}
+
+func (ap *Appender) StartTime() time.Time { return ap.start }
+func (ap *Appender) Close() error         { return ap.f.Close() }
+func (ap *Appender) Append(hit *Hit) error {
+	ap.sb.Reset()
+	ap.sb.WriteString(strconv.FormatInt(hit.Timestamp.Unix(), 10))
+	ap.sb.WriteByte(',')
+	ap.sb.WriteString(hit.URI)
+	ap.sb.WriteByte(',')
+	ap.sb.WriteString(hit.Session)
+	ap.sb.WriteByte(',')
+	ap.sb.WriteString(hit.Ref)
+	ap.sb.WriteByte(',')
+	ap.sb.WriteString(hit.Country)
+	ap.sb.WriteByte(',')
+	ap.sb.WriteString(hit.Device)
+	ap.sb.WriteByte('\n')
+	_, err := ap.f.Write([]byte(ap.sb.String()))
+	if err == nil && ap.start.IsZero() {
+		ap.start = hit.Timestamp
+	}
+	return err
+}
+
+func ParseAppendLog(filename string, location *time.Location) (*Stats, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	sessions := map[string]bool{}
+	stats := &Stats{
+		Interval:  time.Hour,
+		URIs:      Frame{len: 24},
+		Sessions:  Frame{len: 24},
+		Refs:      Frame{len: 24},
+		Countries: Frame{len: 24},
+		Devices:   Frame{len: 24},
+	}
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			break
+		}
+		if len(line) > 0 && line[len(line)-1] == '\n' {
+			line = line[:len(line)-1]
+		}
+		parts := strings.Split(line, ",")
+		if len(parts) != 6 {
+			continue
+		}
+		unix, err := strconv.ParseInt(parts[0], 10, 64)
+		if err != nil {
+			continue
+		}
+		timestamp := time.Unix(unix, 0).In(location)
+		if stats.Start.IsZero() {
+			stats.Start = date(timestamp)
+		}
+		hour := timestamp.Hour()
+		if uri := parts[1]; uri != "" {
+			stats.URIs.Row(uri).Values[hour]++
+		}
+		if sess := parts[2]; sess == "" || !sessions[sess] {
+			sessions[sess] = true
+			stats.Sessions.Row("sessions").Values[hour]++
+			if ref := parts[3]; ref != "" {
+				stats.Refs.Row(ref).Values[hour]++
+			}
+			if cn := parts[4]; cn != "" {
+				stats.Countries.Row(cn).Values[hour]++
+			}
+			if dev := parts[5]; dev != "" {
+				stats.Devices.Row(dev).Values[hour]++
+			}
+		}
+	}
+	return stats, nil
+}
---
+++ log_test.go
@@ -0,0 +1,127 @@
+package nullitics
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestAppender(t *testing.T) {
+	testFile := "_test_append.log"
+	timestamp := time.Unix(123456789, 0)
+	defer os.Remove(testFile)
+
+	// Ensure file does not exist before the tests
+	os.Remove(testFile)
+
+	t.Run("New", func(t *testing.T) {
+		ap, err := NewAppender(testFile, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Time for empty log files should be zero
+		if st := ap.StartTime(); !st.IsZero() {
+			t.Error(st)
+		}
+		// File should be created and have zero size
+		if fi, err := os.Stat(testFile); err != nil || fi.Size() != 0 {
+			t.Error(fi, err)
+		}
+		// Write one record
+		if err := ap.Append(&Hit{Timestamp: timestamp, URI: "/foo"}); err != nil {
+			t.Error(err)
+		}
+		if err := ap.Close(); err != nil {
+			t.Error(err)
+		}
+		// File size should be non-zero
+		if fi, err := os.Stat(testFile); err != nil || fi.Size() < 15 {
+			t.Error(fi, err)
+		}
+	})
+
+	t.Run("Reopen", func(t *testing.T) {
+		ap, err := NewAppender(testFile, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Start time should be the timestamp of the first record
+		if st := ap.StartTime(); st != timestamp {
+			t.Error(st)
+		}
+		// Write another record
+		if err := ap.Append(&Hit{Timestamp: timestamp.Add(time.Second), URI: "/hello"}); err != nil {
+			t.Error(err)
+		}
+		if err := ap.Close(); err != nil {
+			t.Error(err)
+		}
+		b, _ := ioutil.ReadFile(testFile)
+		if string(b) != "123456789,/foo,,,,\n123456790,/hello,,,,\n" {
+			t.Error(string(b))
+		}
+	})
+
+	t.Run("Truncate", func(t *testing.T) {
+		ap, err := NewAppender(testFile, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Start time should be zero again
+		if st := ap.StartTime(); !st.IsZero() {
+			t.Error(st)
+		}
+		if err := ap.Close(); err != nil {
+			t.Error(err)
+		}
+		// File size should be non-zero
+		if fi, err := os.Stat(testFile); err != nil || fi.Size() != 0 {
+			t.Error(fi, err)
+		}
+	})
+}
+func TestLogStats(t *testing.T) {
+	os.RemoveAll("_testdir")
+	c := NewCollector("_testdir", time.UTC)
+	ts := func(s string) time.Time {
+		t, _ := time.Parse("2006-01-02 15:04", s)
+		return t
+	}
+	for _, hit := range []*Hit{
+		{ts("2021-01-01 10:30"), "/a", "", "", "", ""},
+		{ts("2021-01-01 10:42"), "/b", "", "", "", ""},
+		{ts("2021-01-01 12:00"), "/c", "", "", "", ""},
+		{ts("2021-01-01 18:00"), "/c", "", "", "", ""},
+		{ts("2021-01-01 18:01"), "/c", "", "", "", ""},
+		{ts("2021-01-01 18:59"), "/d", "", "", "", ""},
+		{ts("2021-01-02 00:00"), "/e", "", "", "", ""},
+		{ts("2021-01-02 08:00"), "/f", "", "", "", ""},
+		{ts("2021-01-04 09:45"), "/g", "", "", "", ""},
+		{ts("2021-01-05 07:30"), "/h", "", "", "", ""},
+	} {
+		if err := c.Add(hit); err != nil {
+			t.Error(err)
+		}
+	}
+	d, h, err := c.Stats()
+	if err != nil {
+		t.Error(err)
+	}
+	if err := c.Add(&Hit{ts("2021-01-05 00:01"), "/g", "", "", "", ""}); err != nil {
+		t.Error(err)
+	}
+	if err := c.Add(&Hit{ts("2021-01-05 23:59"), "/h", "", "", "", ""}); err != nil {
+		t.Error(err)
+	}
+	d2, h2, err := c.Stats()
+	if err != nil {
+		t.Error(err)
+	}
+	// TODO
+	//t.Log(d)
+	//t.Log(d2)
+	//t.Log(h)
+	//t.Log(h2)
+	_, _, _, _ = d, d2, h, h2
+}
